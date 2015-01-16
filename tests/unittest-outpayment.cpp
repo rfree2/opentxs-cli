4e3e4191ddb21b6a3151fb3bbf63f93cc6c88259
Avvys <avvy@mempo.org>
2015-01-16 10:26:04 +0100

    commandline tests

--- tests/unittest-outpayment.cpp
+++ tests/unittest-outpayment.cpp
@@ -4,20 +4,47 @@
 #include "../src/base/lib_common3.hpp"
 
 #include "../src/base/useot.hpp"
-                                                                                                                                                                                                                                                                                
-using namespace nOT::nUtils;                                                                                                                                                                                                                                                    
-                                                                                                                                                                                                                                                                                
-class cUseOtOutPaymentTest: public testing::Test {                                                                                                                                                                                                                                  
-protected:                                                                                                                                                                                                                                                                      
-        std::shared_ptr<nOT::nUse::cUseOT> useOt;                                                                                                                                                                                                                               
-        virtual void SetUp() {                                                                                                                                                                                     
-                useOt = std::make_shared<nOT::nUse::cUseOT>("outpayment-cheque");                                                                                                                                        
-                                                                                                                                                                                                                                              
-                cout << "outpayment test" << endl;                                                                                                                                                                                                
-        }                                                                                                                                                                                                                                     
-                                                                                                                                                                                                                                              
-        virtual void TearDown() {                                                                                                                                                                                                             
-                cout << "tear down" << endl;                                                                                                                                                                                                  
-        }
+#include "../src/base/cmd.hpp"
+
+using namespace nOT::nUtils;
+
+class cUseOtOutpaymentTest: public testing::Test {
+protected:
+	std::shared_ptr<nOT::nUse::cUseOT> useOt;
+	string nym1;
+	string preCmd;
+
+	virtual void SetUp() {
+		useOt = std::make_shared<nOT::nUse::cUseOT>("outpayment-cheque");
+		nym1 = "FT's Test Nym";
+		preCmd = "ot outpayment ";
+
+		cout << "outpayment test" << endl;
+	}
+
+	virtual void TearDown() {
+		cout << "tear down" << endl;
+	}
 };
 
+TEST_F(cUseOtOutpaymentTest, cmdOk) {
+	shared_ptr<nOT::nNewcli::cCmdParser> parser(new nOT::nNewcli::cCmdParser);
+	parser->Init();
+	auto tmp = parser->StartProcessing("ot outpayment ls", useOt);
+	tmp.UseExecute();
+
+}
+
+TEST_F(cUseOtOutpaymentTest, cmdFail) {
+	shared_ptr<nOT::nNewcli::cCmdParser> parser(new nOT::nNewcli::cCmdParser);
+	parser->Init();
+
+	string cmd = preCmd + "show " + nym1 + " -1";
+	_fact(cmd);
+	EXPECT_ANY_THROW(parser->StartProcessing(cmd, useOt).UseExecute());
+
+	cmd = preCmd + "show " + nym1 + " 1000";
+	_fact(cmd);
+	EXPECT_ANY_THROW(parser->StartProcessing(cmd, useOt).UseExecute());
+
+}
