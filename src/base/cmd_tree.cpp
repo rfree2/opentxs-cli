5249d4756807500f4d6819cd25787000755439a5
Avvys <avvy@mempo.org>
2015-01-19 17:25:55 +0100

    record clear

--- src/base/cmd_tree.cpp
+++ src/base/cmd_tree.cpp
@@ -614,7 +614,11 @@ void cCmdParser::Init() {
 	//======== ot recordbox ========
 
 	AddFormat("record ls", {}, {pAccount, pNym, pServer}, NullMap,
-			LAMBDA { auto &D=*d; return U.RecordDisplay( D.v(1, U.AccountGetName(U.AccountGetDefault())), D.v(2, U.ServerGetName(U.ServerGetDefault())), D.has("--dryrun") ); } );
+			LAMBDA { auto &D=*d; return U.RecordDisplay( D.v(1, U.AccountGetName(U.AccountGetDefault())), D.has("--dryrun") ); } );
+
+	AddFormat("record clear", {}, {pAccount}, { {"--all", pBool} },
+			LAMBDA { auto &D=*d; return U.RecordClear( D.v(1, U.AccountGetName(U.AccountGetDefault())), D.has("--all"), D.has("--dryrun") ); } );
+
 
 	//======== ot server ========
 
