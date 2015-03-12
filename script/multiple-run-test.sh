60760b7d615fbe63cf04293bd4adbf301c34da0e
Avvys <avvy@mempo.org>
2015-03-12 12:43:21 +0100

    better filenames

--- script/multiple-run-test.sh
+++ script/multiple-run-test.sh
@@ -2,8 +2,10 @@
 
 times=$1 
 test_script="printf '_' | script/run-test.sh"
-dir=$(date "+%H%M%S_%m%d%y")
+dir=$(date "+%d-%m-%y_%H%M%S")
+full_dir="test_output/$dir"
 
+mkdir -p "tests_output"
 mkdir -p $dir
 
 function checkExist() { 
@@ -19,11 +21,11 @@ function run() {
 	                                                                                                                                                                                             
 	while [ $i -lt $times ]; do                                                                                                                                                                                                                   
         i=$[$i+1]  
-        name="$dir/test$i.txt"
+        name="$full_dir/test$i.txt"
         echo "test number: $i $name"
-        echo "test number: $i" >> $dir/info.txt
-        eval $test_script --gtest_output=xml:$dir/test_$i.xml   &>  $name                                                                                                                                                                                                                                                                                                                                                                                                                                                             
-		tail $name
+        echo "test number: $i" >> $full_dir/info.txt
+        eval $test_script --gtest_output=xml:$full_dir/$i.xml   &>  $name                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+		tail $name    
         sleep 1
 	done 
 }
